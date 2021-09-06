6ebbabd00d2dc9caac6c21a1afbfa91b86cd5873
zloihach <stud126175@vyatsu.ru>
2021-09-06 16:19:12 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Programm
+    {
+        static void Main(String[] args)
+        {
+            double a = 0, b = 4 * Math.PI, n = Math.PI / 6, f, x;
+            for (x = a;
+                x <= b + n;
+                x += n)
+            {
+                f = x + 1 + Math.Sin(x - 1);
+                Console.WriteLine("x = " + x + " f= " + f);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
